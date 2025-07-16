677052cd30fc4ce1f6d2889143af14d4d84acfb2
Kerby <kerbycanaria@gmail.com>
2025-07-17 05:23:26 +0800

    login ui

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -2,4 +2,6 @@ export const appwriteConfig = {
     endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
     projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
     platform: "com.project.fooddelivery",
+    databaseId: '6878043900395b31dd0f',
+    userCollection: '6878045b000051c690f7',
 }
